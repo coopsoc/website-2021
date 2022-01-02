ed4fe47196ab6ad05ffceb6cac39fa7256f2df3e
hanyuone <hanyuan.work@gmail.com>
2022-01-02 13:36:18 +1100

    fix: remove movie night modal

--- src/views/Charity.js
+++ src/views/Charity.js
@@ -29,7 +29,7 @@ import { START, END, EVENTS } from "./data/CharityData.jsx";
 
 class Charity extends React.Component {
 
-  
+
   constructor(props) {
     super(props);
 
@@ -71,7 +71,7 @@ class Charity extends React.Component {
       goFundMePopUp: !this.state.goFundMePopUp
     });
   }
-  
+
 
   render() {
     return (
@@ -81,7 +81,7 @@ class Charity extends React.Component {
           <Header />
 
           <section className="section">
-            
+
             <Row className="justify-content-center text-center ">
               <Col lg="8">
                 <h1 class="animate__animated animate__zoomIn animate__fast"><h2 className="display-1">CHARITY</h2></h1>
@@ -121,7 +121,6 @@ class Charity extends React.Component {
             </Container> */}
 
           {/* Interactive carousel for events */}
-          
           <section className="section">
             <div class="container"><hr /></div>
             <Row className="justify-content-center text-center mb-lg">
@@ -136,9 +135,8 @@ class Charity extends React.Component {
           </section>
 
           {/* List of supported charities */}
-          
           <section className="section">
-          <div class="container"><hr /></div>
+            <div class="container"><hr /></div>
             <Row className="justify-content-center text-center mb-lg">
               <h2>Charities Supported</h2>
             </Row>
@@ -147,47 +145,19 @@ class Charity extends React.Component {
           </section>
 
           {/* List of upcoming events */}
-          
-          {/* <section className="section">
+          <section className="section">
             <div class="container"><hr /></div>
             <Row className="justify-content-center text-center mb-lg">
               <h2>Upcoming Community Events</h2>
             </Row>
 
             <CharityUpcoming />
-          </section> */}
+          </section>
 
           <CharityModal
             isOpen={this.state.showModal}
             toggle={this.toggleModal}
             event={this.state.currentEvent} />
-
-            <Modal
-              className="modal-dialog-centered"
-              isOpen={this.state.goFundMePopUp}
-              toggle={this.toggleModal}
-            >
-              <div className="modal-body">
-                <Row style={{ padding: "20px" }} className="justify-content-center text-center">
-                <h5>Our upcoming movie night is being run in support of One In Five! Donate to our GoFundMe campaign now!</h5>
-                </Row>
-                <Row className="justify-content-center text-center">
-                  <div class="gfm-embed" data-url="https://www.gofundme.com/f/Co-op-Soc-charity-movie-night/widget/large/"></div>
-                </Row>
-              </div>
-              <div className="modal-footer">
-                <Button
-                  className="ml-auto"
-                  color="link"
-                  data-dismiss="modal"
-                  type="button"
-                  onClick={() => this.toggleModal("goFundMePopUp")}
-                >
-                  Close
-                </Button>
-              </div>
-            </Modal>
-            
         </main>
         <SimpleFooter />
       </>
