bdc1cadefcfcb235e3d6f85271906afa7cd3b862
Hanyuan Li <hanyuan.work@gmail.com>
2022-04-24 19:20:10 +1000

    feat(podcast): add stub podcast page

---
+++ src/views/Podcast.jsx
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+
+import Navigation from "components/Navigation.jsx";
+import Header from "components/Header.jsx";
+import Footer from "components/Footer.jsx";
+
+const Podcast = () => {
+  const mainRef = useRef();
+  
+  return (
+    <>
+      <Navigation />
+        <main ref={mainRef}>
+          <Header />
+        </main>
+        <Footer />
+      </>
+  );
+};
+
+export default Podcast;
